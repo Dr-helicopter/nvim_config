603be792df6eced0fdf8f538861aadd60674c8cb
Dr-helicopter <hashmya1@gmail.com>
2024-11-13 17:13:59 +0330

    finally its fixed

--- lua/config/noice.lua
+++ lua/config/noice.lua
@@ -2,21 +2,12 @@
 
 local noice = require("noice")
 noice.setup({
+    enabled = true,
+    view = 'cmdline_popup',        
     cmdline = {
-		formats = {
-			cmdline = { pattern = "^:", icon = "", lang = "vim" },
+		format = {
+			cmdline = { pattern = "^:", icon = ":>", lang = "vim" },
         },
-        enabled = true,
-        view = 'cmdline_popup',        
-    },
-    
-	presets = {
-        bottom_search = true,            -- use a classic bottom cmdline for search
-        command_palette = true,          -- position the cmdline and popupmenu together
-        long_message_to_split = true,    -- long messages will be sent to a split
-        inc_rename = false,              -- enables an input dialog for inc-rename.nvim
-        lsp_doc_border = false,          -- add a border to hover docs and signature help
     },
 })
 
-print(noice)
